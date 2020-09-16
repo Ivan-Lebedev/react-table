27835f06e4ec4917acb0cbf334ee3cfaaa662184
ivan <ivieee@yandex.ru>
2020-09-16 23:31:00 +0400

    DetailedRpwView undefined user bug fix

--- src/DetailRowView/DetailRowView.js
+++ src/DetailRowView/DetailRowView.js
@@ -1,15 +1,23 @@
 import React from 'react'
 
-export default ({ person }) => (
-    <div>
-        <p>Selected User <b>{person.firstName} {person.lastName}</b></p>
-        <p>
-            Description: <br />
-        </p>
-        <p>{person.description}</p>
-        <p>Address: <b>{person.address.streetAddress}</b></p>
-        <p>City: <b>{person.address.city}</b></p>
-        <p>State: <b>{person.address.state}</b></p>
-        <p>Zip: <b>{person.address.zip}</b></p>
-    </div>
-)
+export default ({ person }) => {
+    const userData = (person.address && person.description)
+        ? (
+            <div>
+                <p>Selected User <b>{person.firstName} {person.lastName}</b></p>
+                <p><b>Description: </b>{person.description}</p>
+                <p>Address: <b>{person.address.streetAddress}</b></p>
+                <p>City: <b>{person.address.city}</b></p>
+                <p>State: <b>{person.address.state}</b></p>
+                <p>Zip: <b>{person.address.zip}</b></p>
+            </div>
+        )
+        : (
+            <div>
+                <p>Selected User <b>{person.firstName} {person.lastName}</b></p>
+                <p><b>No detailed data</b></p>
+            </div>
+        )
+
+    return userData
+}
